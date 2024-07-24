8ee483479a3a51f28e86e13f5d6d061527ced1d8
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-24 16:14:25 -0300

    init project

---
+++ src/main/java/com/pastelaria/entities/Pastel.java
@@ -0,0 +1,5 @@
+package com.pastelaria.entities;
+
+public class Pastel {
+
+}
---
+++ src/test/java/com/pastelaria/PastelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
