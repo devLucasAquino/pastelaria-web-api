c0f9cd793165cb887354d0060aba55785105e43e
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-24 16:31:38 -0300

    feat: criando entidade, repository e controller

--- src/main/java/com/pastelaria/entities/Pastel.java
+++ src/main/java/com/pastelaria/entities/Pastel.java
@@ -1,5 +1,48 @@
 package com.pastelaria.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PASTEL")
 public class Pastel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sabor;
+	private Double preco;
+	
+	public Pastel() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
 
 }
